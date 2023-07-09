c94d1cfaf491d92ba6388db1f73798ee8b372ec7
Karhapoltseva Yaliya <kargapoltseva1993@gmail.com>
2023-07-09 13:24:48 +0300

    commit "create HW_3"

---
+++ src/main/java/hw_3/BurgerMain.java
@@ -0,0 +1,11 @@
+package hw_3;
+
+public class BurgerMain {
+    public static void main(String[] args) {
+
+        Burger burger = new Burger("Bun","Meat","Chesse","Salute","Mayonnaise");
+        Burger burgerDiet = new Burger("Bun","Meat","Chesse","Salute");
+        Burger burgerMeat = new Burger("Bun",true,"Meat","Chesse",
+                "Salute","Mayonnaise");
+    }
+}
