0c8b5a072e232b39e3e72822517b4c759ec21a98
Karhapoltseva Yaliya <kargapoltseva1993@gmail.com>
2023-07-12 18:20:36 +0300

    last try HW_3

--- src/main/java/hw_3/Burger.java
+++ src/main/java/hw_3/Burger.java
@@ -1,28 +1,40 @@
 package hw_3;
 
 public class Burger {
-        private BunType bun;
-        private CheeseType cheese;
-        private GreenType green;
-        private MeatType meat;
-        private int counterMeat;
-        private boolean mayonnaise;
-
-    public Burger() {
-        this.bun = bun.WHEATBUN;
-        this.cheese = cheese.MILKCHEESE;
-        this.green = green.GREENERY;
+    private  BunType bun;
+    private  CheeseType cheese;
+    private  GreenType green;
+    private  MeatType meat;
+    private  int counterMeat;
+    private  boolean mayonnaise;
+        //diet burger
+    public Burger(BunType bun,CheeseType cheese,GreenType green,MeatType meat){
+         this.bun = bun;
+         this.cheese = cheese;
+         this.green = green;
+         this.meat = meat;
+         this.counterMeat=1;
     }
-    public Burger(MeatType meat, int counterMeat,boolean mayonnaise){
-        this();
-        this.meat = meat.BEEF;
-        this.counterMeat=2;
-        this.mayonnaise= true;
+    //burgerDouble or original
+    public Burger(int counterMeat){
+        this(BunType.BRANBUN,CheeseType.HARDCHEESE,GreenType.SALAD,MeatType.BEEF);
+        this.counterMeat = (counterMeat==2) ? 2 : 1;
+        this.mayonnaise=false;
     }
-    public Burger(MeatType meat, int counterMeat){
-        this();
-        this.meat = meat.CHICKEN;
-        this.counterMeat=1;
+    //burgerDouble or original
+    public Burger (int counterMeat, boolean mayonnaise){
+        this(BunType.WHEATBUN,CheeseType.MILKCHEESE,GreenType.GREENERY,MeatType.PORK);
+        this.counterMeat = (counterMeat==2) ? 2 : 1;
+        this.mayonnaise=true;
+    }
+        //burger oll
+    public Burger(BunType bun, CheeseType cheese, GreenType green, MeatType meat,int counterMeat, boolean mayonnaise){
+        this.bun = bun;
+        this.cheese = cheese;
+        this.green = green;
+        this.meat = meat;
+        this.counterMeat = counterMeat;
+        this.mayonnaise = mayonnaise;
     }
 
     @Override
--- src/main/java/hw_3/BurgerMain.java
+++ src/main/java/hw_3/BurgerMain.java
@@ -3,12 +3,17 @@ package hw_3;
 public class BurgerMain {
     public static void main(String[] args) {
 
-        Burger burger = new Burger(Burger.MeatType.BEEF,1);
-        Burger burgerDouble = new Burger(Burger.MeatType.CHICKEN,2,true);
-        Burger burgerDiet = new Burger();
+        Burger burgerOll = new Burger(null,null,null, null,8,false);
+        Burger burgerDiet = new Burger(Burger.BunType.WHEATBUN, Burger.CheeseType.MILKCHEESE,
+                Burger.GreenType.GREENERY, Burger.MeatType.CHICKEN);
+        Burger burgerRegular = new Burger(1);
+        Burger burgerRegular2 = new Burger(1,true);
+        Burger burgerDouble = new Burger(2,true);
 
-        System.out.println(burger.toString());
-        System.out.println(burgerDouble.toString());
+        System.out.println(burgerOll.toString());
         System.out.println(burgerDiet.toString());
+        System.out.println(burgerRegular.toString());
+        System.out.println(burgerRegular2.toString());
+        System.out.println(burgerDouble.toString());
     }
 }
